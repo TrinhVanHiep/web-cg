e75d5c5a88a5562847d38579fc69aba96075bf89
Trịnh Văn Hiệp <hieptv@hieptv.local>
2024-04-25 11:16:49 +0700

    update home page

---
+++ assets/feedback-customer.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var el = $('.slider-feedback');
+
+    var carousel;
+    var carouselOptions = {
+        margin: 30,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            991: {
+                items: 3,
+            }
+        }
+    };
+});
